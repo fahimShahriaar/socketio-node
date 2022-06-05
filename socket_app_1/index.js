ba9b50e82c7792aac1cd3f4363513e77d208bbb7
Shahriar <fem.nwu@gmail.com>
2022-06-05 16:41:38 +0600

    update code

--- socket_app_1/index.js
+++ socket_app_1/index.js
@@ -2,15 +2,39 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+const { color, log } = require("console-log-colors");
+const { red, green, cyan } = color;
+
 const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
 io.on("connection", (socket) => {
-  console.log("User Connected");
+  log.white(color.greenBG("User Connected..."));
+
+  setInterval(() => {
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    socket.send(`Welcome to the chat room! ${time}`);
+  }, 100);
+
+  setInterval(() => {
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    socket.emit("privateMessage", time);
+  }, 100);
+
+  socket.on("message", (message) => {
+    console.log(`${message}`);
+  });
+
+  socket.on("myEvent", (message) => {
+    console.log("myEvent");
+    console.log(`${message}`);
+  });
 
   socket.on("disconnect", () => {
-    console.log("User Disconnected");
+    log.white(color.redBG("User Disconnected..."));
   });
 });
 
